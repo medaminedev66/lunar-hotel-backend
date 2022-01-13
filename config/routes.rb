9ce186e0656b272e1f1c836b496f7b5f6776f6ac
Juliana Ifionu <julieifionu@gmail.com>
2022-01-13 15:12:05 +0100

    Fix routing error

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, defaults: { format: :json }, path: '',
                      path_names: { sign_in: 'login', sign_out: 'logout', registration: 'signup' }
 
-  defaults format :json do
+  defaults format: :json do
     resources :rooms, only: %i[create index show update destroy]
     resources :reservations, only: %i[create index show update destroy]
   end
