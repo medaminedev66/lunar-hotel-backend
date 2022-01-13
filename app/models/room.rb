5d03480c6f15ba4882d6280027f17b3723ec7663
Juliana Ifionu <julieifionu@gmail.com>
2022-01-13 16:31:28 +0100

    Fix has many relations

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,9 +1,9 @@
 class Room < ApplicationRecord
   validates :name, :city, length: { maximum: 50 }
-  validates :name, :city, :rate, :type, :amenities, :picture, presence: true
+  validates :name, :city, :rate, :room_type, :amenities, :picture, presence: true
   validates :rate, numericality: { greater_than_or_equal_to: 0 }
 
   belongs_to :user
-  belongs_to :reservation
-  has_one_attached :picture
+  has_many :reservations, dependent: :destroy
+  # has_one_attached :picture
 end
