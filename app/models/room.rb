7a616545e91d371b683a9cb21fcca976439f0554
Daniel M. Matongo <m_matongo@icloud.com>
2022-01-13 12:16:30 +0200

    add models with logic

---
+++ app/models/room.rb
@@ -0,0 +1,8 @@
+class Room < ApplicationRecord
+  validates :name, :city, length: { maximum: 50 }
+  validates :name, :city, :rate, :type, :amenities, :picture, presence: true
+  validates :rate, numericality: { greater_than_or_equal_to: 0 }
+
+  belongs_to :user
+  belongs_to :reservation
+end
