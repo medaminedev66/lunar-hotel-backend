4738a60c422e5e30afde3af829adbc567ec1c32a
Daniel M. Matongo <m_matongo@icloud.com>
2022-01-13 15:45:45 +0200

    add controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
+  respond_to :json
+
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
