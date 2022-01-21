1fe46d3acd445873ca49422f7fe5053303466625
Adedamola Shittu <shittuadedamola@rocketmail.com>
2022-01-21 18:06:25 +0100

    Remove Configured Parameters Method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,5 @@ class ApplicationController < ActionController::API
   respond_to :json
 
   before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password password_confirmation])
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
-  end
 end
