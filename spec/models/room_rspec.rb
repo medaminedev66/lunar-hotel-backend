83aa4f46a98004519e72816f350a5fb1a7057225
Amine Smahi <md.amine.smahi@gmail.com>
2022-01-23 11:33:37 +0100

    Test the creation of the model

--- spec/models/room_rspec.rb
+++ spec/models/room_rspec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Room, type: :model do
+  let(:user) { User.create(name: 'Amine', email: 'example@mail.com', password: 'password') }
+  let(:room) { Room.create(user_id: user.id, name: 'See view', picture: 'pic.png', city: 'Rabat', rate: 4, room_type: "Master", amenities:  'swimming pool') }
 
+  describe 'Validations' do
+    context 'when valid' do
+      it { expect(room).to be_valid }
+    end
+  end
 end    
