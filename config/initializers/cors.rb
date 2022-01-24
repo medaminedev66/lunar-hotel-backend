f2f0e12dff6b585c1f6f1eff1d7851694db075e2
Daniel M. Matongo <mmatongo_@hotmail.com>
2022-01-24 12:18:45 +0200

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'http://localhost:3001', 'http://127.0.0.1:3001'
     resource '*', headers: %w(Authorization), expose: %w(Authorization), methods: :any, max_age: 600 
   end
 end
