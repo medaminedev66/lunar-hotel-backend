1e149cecfa8d92f9e96f32b91209626a25de0615
Shittu Adedamola <shittuadedamola@rocketmail.com>
2022-01-25 19:06:16 +0100

    Write Request Tests For Sessions Endpoint

---
+++ spec/requests/devise/sessions_spec.rb
@@ -0,0 +1,51 @@
+require 'swagger_helper'
+
+RSpec.describe 'devise/sessions', type: :request do
+  path '/users/login' do
+    post('create session') do
+      consumes 'application/json'
+      parameter name: :login, in: :body, schema: {
+        type: :object,
+        properties: {
+          email: { type: :string },
+          password: { type: :string }
+        },
+        required: %w[email password]
+      }
+      response(200, 'successful') do
+        let(:login) { { email: 'admin@admin.com', password: '2435647' } }
+        run_test!
+        # after do |example|
+        #   example.metadata[:response][:content] = {
+        #     'application/json' => {
+        #       example: JSON.parse(response.body, symbolize_names: true)
+        #     }
+        #   }
+        # end
+        # run_test!
+      end
+
+      response '422', 'invalid request' do
+        let(:login) { { email: 'admin@admin.com', password: '2435' } }
+        run_test!
+      end
+    end
+  end
+
+  path '/users/logout' do
+    delete('delete session') do
+      security [bearer_auth: []]
+      response(200, 'successful') do
+        response '201', 'Logged Out' do
+          let(:Authorization) { "Bearer #{::Base64.strict_encode64('admin@admin.com:2435647')}" }
+          run_test!
+        end
+
+        response '401', 'authentication failed' do
+          let(:Authorization) { "Bearer #{::Base64.strict_encode64('bogus:bogus')}" }
+          run_test!
+        end
+      end
+    end
+  end
+end
