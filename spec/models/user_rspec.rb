5f23880b09ff746b21c3f560dd4e870305a6bcd4
medaminedev66 <md.amine.smahi@gmail.com>
2022-01-25 13:23:06 +0100

    The user name should not nil

--- spec/models/user_rspec.rb
+++ spec/models/user_rspec.rb
@@ -1,4 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  subject do
+    User.new(name: 'Amine', email: 'example@mail.com', password: 'password')
+  end
+  before { subject.save }
+
+  it 'name should not be nil' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
 end
