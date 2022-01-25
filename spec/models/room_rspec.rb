3eb476f1052be070d37d282129d346b4dde65ecf
medaminedev66 <md.amine.smahi@gmail.com>
2022-01-25 12:06:18 +0100

    Room should validate the rate

--- spec/models/room_rspec.rb
+++ spec/models/room_rspec.rb
@@ -14,4 +14,19 @@ RSpec.describe Room, type: :model do
     room.name = nil
     expect(room).to_not be_valid
   end
+
+   it 'should allow valid name' do
+      room.name = 'Apple'
+      expect(room).to be_valid
+    end
+
+    it 'should validate rate' do
+      room.rate = -5
+      expect(room).to_not be_valid
+    end
+
+    it 'should validate rate' do
+      room.rate = 10
+      expect(room).to be_valid
+    end
 end    
