7b170a2a8525c5acfad989b0ee97530e76593fea
Shittu Adedamola <shittuadedamola@rocketmail.com>
2022-01-25 19:05:56 +0100

    Write Request Tests For Registrations Endpoint

---
+++ spec/requests/devise/registrations_spec.rb
@@ -0,0 +1,28 @@
+require 'swagger_helper'
+
+RSpec.describe 'devise/registrations', type: :request do
+  path '/users/signup' do
+    post('create registration') do
+      consumes 'application/json'
+      parameter name: :signup, in: :body, schema: {
+        type: :object,
+        properties: {
+          name: { type: :string },
+          email: { type: :string },
+          password: { type: :string }
+        },
+        required: %w[name email password]
+      }
+      response(200, 'successful') do
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+end
