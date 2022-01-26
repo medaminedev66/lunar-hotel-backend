18048cf4dba97cb590a831c23e02e6b83e95fee0
Juliana Ifionu <julieifionu@gmail.com>
2022-01-26 11:58:20 +0100

    Write spec to delete reservation

--- spec/requests/api/v1/reservations_spec.rb
+++ spec/requests/api/v1/reservations_spec.rb
@@ -110,19 +110,31 @@ RSpec.describe 'api/v1/reservations', type: :request do
       end
     end
 
-  #   delete('delete reservation') do
-  #     response(200, 'successful') do
-  #       let(:id) { '123' }
+    delete('delete reservation') do
+      tags 'Reservations'
+      security [bearer_auth: []]
+      response(200, 'successful') do
+        let(:id) { '123' }
 
-  #       after do |example|
-  #         example.metadata[:response][:content] = {
-  #           'application/json' => {
-  #             example: JSON.parse(response.body, symbolize_names: true)
-  #           }
-  #         }
-  #       end
-  #       run_test!
-  #     end
-  #   end
-   end
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+
+      response '201', 'delete reservation' do
+        let(:Authorization) { "Bearer #{::Base64.strict_encode64('admin@admin.com:2435647')}" }
+        run_test!
+      end
+
+      response '401', 'authentication failed' do
+        let(:Authorization) { "Bearer #{::Base64.strict_encode64('bogus:bogus')}" }
+        run_test!
+      end
+    end
+  end
 end
