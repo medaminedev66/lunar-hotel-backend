6eb925fd1bd5617e1f815f95a4fe996081943a07
Juliana Ifionu <julieifionu@gmail.com>
2022-01-26 11:52:35 +0100

    Write spec to get single reservation

--- spec/requests/api/v1/reservations_spec.rb
+++ spec/requests/api/v1/reservations_spec.rb
@@ -78,25 +78,37 @@ RSpec.describe 'api/v1/reservations', type: :request do
         run_test!
       end
     end
+  end
 
-  # path '/api/v1/reservations/{id}' do
-  #   # You'll want to customize the parameter types...
-  #   parameter name: 'id', in: :path, type: :string, description: 'id'
+  path '/api/v1/reservations/{id}' do
+    # You'll want to customize the parameter types...
+    parameter name: 'id', in: :path, type: :string, description: 'id'
 
-  #   get('show reservation') do
-  #     response(200, 'successful') do
-  #       let(:id) { '123' }
+    get('show reservation') do
+      tags 'Reservations'
+      security [bearer_auth: []]
+      response(200, 'successful') do
+        let(:id) { '123' }
 
-  #       after do |example|
-  #         example.metadata[:response][:content] = {
-  #           'application/json' => {
-  #             example: JSON.parse(response.body, symbolize_names: true)
-  #           }
-  #         }
-  #       end
-  #       run_test!
-  #     end
-  #   end
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+      response '201', 'successfully authenticated' do
+        let(:Authorization) { "Bearer #{::Base64.strict_encode64('admin@admin.com:2435647')}" }
+        run_test!
+      end
+
+      response '401', 'authentication failed' do
+        let(:Authorization) { "Bearer #{::Base64.strict_encode64('bogus:bogus')}" }
+        run_test!
+      end
+    end
 
   #   delete('delete reservation') do
   #     response(200, 'successful') do
