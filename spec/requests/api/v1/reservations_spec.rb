342e7700655a7d3bd689cc69e7630569e6550cf1
Juliana Ifionu <julieifionu@gmail.com>
2022-01-26 11:33:21 +0100

    Write spec to get all reservations

--- spec/requests/api/v1/reservations_spec.rb
+++ spec/requests/api/v1/reservations_spec.rb
@@ -5,6 +5,9 @@ RSpec.describe 'api/v1/reservations', type: :request do
   path '/api/v1/reservations' do
 
     get('list reservations') do
+      tags 'Reservations'
+      security [bearer_auth: []]
+
       response(200, 'successful') do
 
         after do |example|
@@ -16,55 +19,65 @@ RSpec.describe 'api/v1/reservations', type: :request do
         end
         run_test!
       end
-    end
 
-    post('create reservation') do
-      response(200, 'successful') do
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
+      response '201', 'Authorized' do
+        let(:Authorization) { "Bearer #{::Base64.strict_encode64('admin@admin.com:2435647')}" }
         run_test!
       end
-    end
-  end
 
-  path '/api/v1/reservations/{id}' do
-    # You'll want to customize the parameter types...
-    parameter name: 'id', in: :path, type: :string, description: 'id'
-
-    get('show reservation') do
-      response(200, 'successful') do
-        let(:id) { '123' }
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
+      response '401', 'authentication failed' do
+        let(:Authorization) { "Bearer #{::Base64.strict_encode64('bogus:bogus')}" }
         run_test!
       end
     end
 
-    delete('delete reservation') do
-      response(200, 'successful') do
-        let(:id) { '123' }
+  #   post('create reservation') do
+  #     response(200, 'successful') do
 
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
-    end
-  end
+  #       after do |example|
+  #         example.metadata[:response][:content] = {
+  #           'application/json' => {
+  #             example: JSON.parse(response.body, symbolize_names: true)
+  #           }
+  #         }
+  #       end
+  #       run_test!
+  #     end
+  #   end
+  # end
+
+  # path '/api/v1/reservations/{id}' do
+  #   # You'll want to customize the parameter types...
+  #   parameter name: 'id', in: :path, type: :string, description: 'id'
+
+  #   get('show reservation') do
+  #     response(200, 'successful') do
+  #       let(:id) { '123' }
+
+  #       after do |example|
+  #         example.metadata[:response][:content] = {
+  #           'application/json' => {
+  #             example: JSON.parse(response.body, symbolize_names: true)
+  #           }
+  #         }
+  #       end
+  #       run_test!
+  #     end
+  #   end
+
+  #   delete('delete reservation') do
+  #     response(200, 'successful') do
+  #       let(:id) { '123' }
+
+  #       after do |example|
+  #         example.metadata[:response][:content] = {
+  #           'application/json' => {
+  #             example: JSON.parse(response.body, symbolize_names: true)
+  #           }
+  #         }
+  #       end
+  #       run_test!
+  #     end
+  #   end
+   end
 end
