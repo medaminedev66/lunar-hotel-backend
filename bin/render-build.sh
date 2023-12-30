79f7c6d540a191a864054a71c97a01ee70c66e3f
Amine Smahi <md.amine.smahi@gmail.com>
2023-12-30 10:15:38 +0100

    Add Webpacker commands to render-build file

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,6 +4,8 @@ set -o errexit
 
 # bundle update -- bundler
 bundle install
+bundle exec rails webpacker:install
+bundle exec rails webpacker:compile
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
