939e5146dd6bd56148710add1766c7278076db16
Amine Smahi <md.amine.smahi@gmail.com>
2023-12-30 11:58:16 +0100

    Remove more files not related to webpacker

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 # bundle update -- bundler
 bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
+# bundle exec rake assets:precompile
+# bundle exec rake assets:clean
 bundle exec rake db:migrate
 bundle exec rake db:seed
